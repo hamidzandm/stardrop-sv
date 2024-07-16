267419e6e8500362a30e9a074109246eb33bc85e
hamidzandm <168014173+hamidzandm@users.noreply.github.com>
2024-07-16 03:19:47 -0400

    Add files via upload

--- llm_langchain.ts
+++ llm_langchain.ts
@@ -412,7 +412,7 @@ export async function generateScheduleExplanation(expansion) {
 
   Generate in a JSON format with date key and description value
   
-  Make the description brief and generate it in less than 30 seconds
+  Make the description very brief and generate it in less than 30 seconds
   
   it us very important to only print the JSON code.
   
